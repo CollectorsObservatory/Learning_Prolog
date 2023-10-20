fed1c8e61018ddbdd0521959595d2f0a4b9bbb28
Fares Majdoub <126903635+CollectorsObservatory@users.noreply.github.com>
2023-10-20 18:16:48 -0400

    Update first_test.pl

--- first_test.pl
+++ first_test.pl
@@ -1,10 +1,21 @@
-% on s'en fiche de la valeur de "homme" , n'importe quoi sera vrai 
+% On se fiche de la valeur de "homme", n'importe quoi sera vrai
 homme(_).
-% Si un homme est toujours vrai alors qq soit un homme , il est mortel
-mortel(X):-homme(X).  
-% un autre programme ou l'homme est socrate
+
+% Si une personne est un homme, alors elle est mortelle
+mortel(X) :- homme(X).  
+
+% Définir un homme spécifique, Socrate
 homme(socrate).
-% si socrate est un homme(toujours vrai) alors il est mort aujourd'hui
-mort(socrate):-homme(socrate).
-% si socrate est mort , alors il n'est pas vivant
-vivant(socrate) :- not( mort(socrate)).
+
+% Si Socrate est un homme (toujours vrai), alors il est mort
+mort(socrate) :- homme(socrate).
+
+% Si Socrate est mort, alors il n'est pas vivant
+vivant(socrate) :- \+ mort(socrate).
+
+% Relations d'amitié entre personnes
+amis(pers0, pers1).
+amis(pers0, pers2).
+
+% Si deux personnes ont un ami commun, alors elles sont amies entre elles
+amis(Z, Y) :- amis(X, Z), amis(X, Y).
