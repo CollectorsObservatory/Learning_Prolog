c793b9cdf772f93ae0cfdf1947a012982889ca07
Fares Majdoub <126903635+CollectorsObservatory@users.noreply.github.com>
2023-10-19 23:29:37 -0400

    Update and rename first_program.pl to first_test.pl

--- first_program.pl
+++
@@ -1,4 +0,0 @@
-% on s'en fiche de la valeur de "homme" , n'importe quoi sera vrai 
-homme(_).
-% Si un homme est toujours vrai alors qq soit un homme , il est mortel
-mortel(X):-homme(X).  
---
+++ first_test.pl
@@ -0,0 +1,10 @@
+% on s'en fiche de la valeur de "homme" , n'importe quoi sera vrai 
+homme(_).
+% Si un homme est toujours vrai alors qq soit un homme , il est mortel
+mortel(X):-homme(X).  
+% un autre programme ou l'homme est socrate
+homme(socrate).
+% si socrate est un homme(toujours vrai) alors il est mort aujourd'hui
+mort(socrate):-homme(socrate).
+% si socrate est mort , alors il n'est pas vivant
+vivant(socrate) :- \+ mort(socrate).
