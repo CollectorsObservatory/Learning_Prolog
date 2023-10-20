890a1b840f97d11966d9e00df0789e7963d5fc35
Fares Majdoub <126903635+CollectorsObservatory@users.noreply.github.com>
2023-10-19 22:53:59 -0400

    Update first_program.pl

--- first_program.pl
+++ first_program.pl
@@ -1,2 +1,4 @@
+% on s'en fiche de la valeur de "homme" , n'importe quoi sera vrai 
 homme(_).
-mortel(X):-homme(X).
+% Si un homme est toujours vrai alors qq soit un homme , il est mortel
+mortel(X):-homme(X).  
